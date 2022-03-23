bdbdb2fc7b24d94ca5897d50fa4778622a76954a
Michael Wright <m.wright92@icloud.com>
2022-03-23 13:19:28 -0700

    add js functionality to complete forms for subtract, multiply, and divide
    Co-authored-by: Dylan Peterson <dylan.peterson17@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -26,5 +26,29 @@ $(document).ready(function() {
     const result = add(number1, number2);
     $("#outputAdd").text(result);
   });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#outputSub").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    $("#outputMult").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#div1").val());
+    const number2 = parseInt($("#div2").val());
+    const result = divide(number1, number2);
+    $("#outputDiv").text(result);
+  });
 });
 
